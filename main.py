31ea318d8597d94949fc70e449bc3fc2df00d58a
drku120 <mitulinskityler@gmail.com>
2023-11-01 08:56:54 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import random
+random.randint(1,9)
+random_number = random.randint(1,9)
+name="tyler"
+Question="will i be in love with someone in 2 years"
+answer = ""
+print(random_number)
+if random_number ==1:
+  answer = "yes definitely"
+if random_number ==2:
+    answer = "it is decidedly so"
+if random_number ==3:
+      answer = "without a doubt so" 
+if random_number ==4:
+      answer = "hazy, try again"
+if random_number ==5:
+      answer= "ask again later"
+if random_number ==6:
+        answer = "better not tell you now"
+if random_number ==7:
+        answer = "my sources say no"
+if random_number ==8:
+          answer = "outlook not so good"
+if random_number==9: 
+          answer = "very doubtful"
+
+print(answer)    
